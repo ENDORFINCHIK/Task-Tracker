f2e9b93435e7a6b4cc735b5b621e89cb5d0b49b7
ENDORFINCHIK <sorokinoleg779@gmail.com>
2025-10-01 00:11:05 +0300

    New patch

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 const table = document.getElementById('table');
+
+const addBtn = document.getElementById('addTaskBtn');
+
+function createTask(){
+    const name = document.getElementById('nameTask').value;
+    const date = document.getElementById('dateTask').value;
+    const status = document.getElementById('statusTask').value;
+    const comment = document.getElementById('commentTask').value;
+    if(name === '' || date === '' || status === '' || comment === ''){
+        alert('Please fill in all fields');
+        return;
+    }
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${name}</td>
+        <td>${date}</td>
+        <td>
+          <select id="statusTask" selected="${status}">
+            <option value="do">In progress</option>
+            <option value="done">Done</option>
+            <option value="started">Not started</option>
+          </select></td>
+        <td><input type="text" value="${comment}"></td>
+    `;
+    table.appendChild(row);
+    document.getElementById('nameTask').value = '';
+    document.getElementById('dateTask').value = '';
+    document.getElementById('statusTask').value = '';
+    document.getElementById('commentTask').value = '';
+}
+
+addBtn.addEventListener('click', createTask);
